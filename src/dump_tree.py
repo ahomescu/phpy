58ecb6c56031afd0ba9aa8ed1b6c76211a2e9039
asuhan <alex.suhan@gmail.com>
2010-10-31 01:15:23 -0700

    test tree grammar as well

--- src/dump_tree.py
+++ src/dump_tree.py
@@ -3,6 +3,7 @@ import antlr3
 import antlr3.tree
 import PhpLexer
 import PhpParser
+import PhpTree
 
 char_stream = antlr3.ANTLRInputStream(sys.stdin)
 lexer = PhpLexer.PhpLexer(char_stream)
@@ -12,3 +13,8 @@ parser = PhpParser.PhpParser(tokens)
 r = parser.program()
 print r.tree.toStringTree()
 
+root = r.tree
+nodes = antlr3.tree.CommonTreeNodeStream(root)
+nodes.setTokenStream(tokens)
+phpTree = PhpTree.PhpTree(nodes)
+phpTree.program()
